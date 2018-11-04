75277913f62a5467030e3be0b62289783eb4f652
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-11-04 02:47:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any?{ |calls| 
+  planeteer_calls.any?{ |calls| calls.length > 4
+  } 
 end
 
 def find_the_cheese# code an argument here

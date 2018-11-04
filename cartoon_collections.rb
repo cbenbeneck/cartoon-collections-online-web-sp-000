015fd80a6920cd98e001f85630fb535206aa9ec4
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-11-04 02:20:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+def roll_call_dwarves(dwarves)
+puts "#{dwarves.each_with_index {|index, name} 
+
+
+end 
+
 
 def summon_captain_planet# code an argument here
   # Your code here

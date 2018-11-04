7602e6f8df898f6e957726a8b25db5f1721bd55d
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-11-04 02:35:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,7 @@ dwarves.each_with_index{ |name, index|
 end 
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map!{ |call|
-  puts "#{call.capitialize}!"
+  planeteer_calls.map!{ |call| call.capitialize
   }
 end
 
